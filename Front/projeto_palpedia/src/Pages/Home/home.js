124769d9dd8c17ca3189fe84cda9ef0ba94bf1ed
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-02-02 12:55:53 -0300

    Start of PalCard

--- Front/projeto_palpedia/src/Pages/Home/home.js
+++ Front/projeto_palpedia/src/Pages/Home/home.js
@@ -1,9 +1,12 @@
 import GameMap from "../../Components/GameMapComponent";
 
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 export default function HomePage() {
-    return(
-        <>
-            {/* <GameMap /> */}
-        </>
-    )
+  return (
+    <>
+    </>
+  );
 }
